7b0ae7b1e383f1d4b730598858a4af101c50d679
alec <alecrondel@hotmail.com>
2023-04-15 16:16:23 -0300

    Criação da tela de registro de horas do colaborador, assim como do Controller, via javafx e scenebuilder

---
+++ Front-End/registrarHoras/src/App.java
@@ -0,0 +1,44 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // CÓDIGO PARA TESTE JAVAFX
+        // stage.setTitle("2rpnet");
+        // Button botao = new Button("clique aqui");
+        // botao.setOnAction(new EventHandler<ActionEvent>() {
+            
+        //     @Override
+        //     public void handle(ActionEvent event) {
+        //         System.out.println("Clicou no botao");
+        //     }
+
+        // });
+
+        // StackPane root = new StackPane();
+        // root.getChildren().addAll(botao);
+        // stage.setScene(new Scene(root, 250, 400, false, null));
+        // stage.show();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("tela.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+
+        stage.setTitle("2rpnet");
+        stage.setScene(tela);
+        stage.show();
+
+    }
+
+}
---
+++ Front-End/registrarHoras/src/telaController.java
@@ -0,0 +1,47 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class telaController {
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private Button btnConfirmar;
+
+    @FXML
+    private Button btnConsultar;
+
+    @FXML
+    private Button btnRegistrarHora;
+
+    @FXML
+    private DatePicker campoData;
+
+    @FXML
+    private TextField campoHoraFinal;
+
+    @FXML
+    private TextField campoHoraInicial;
+
+    @FXML
+    private TextField campoJustificativa;
+
+    @FXML
+    private ChoiceBox<?> campoTipo;
+
+    @FXML
+    void cancelarRegistroHora(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmarRegistroHora(ActionEvent event) {
+
+    }
+
+}
