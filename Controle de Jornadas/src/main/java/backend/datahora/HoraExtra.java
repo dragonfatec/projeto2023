a5c7d22713c85b56fb6593827cdd0cf1db6102d7
Lukas Fernando <lkfernando12345@gmail.com>
2023-04-20 21:15:49 -0300

    Hora extra (inicio da classe)

---
+++ Controle de Jornadas/src/main/java/backend/datahora/HoraExtra.java
@@ -0,0 +1,20 @@
+package backend.datahora;
+
+public class HoraExtra {
+    private String dataInicio;
+    private String dataFim;
+    private Usuario user;
+
+    public HoraExtra(String dataInicio, String dataFim) {
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+    }
+
+    public setUsuario(){
+        this.user = usuario;
+    }
+
+    public lancarHoraNoBanco(){
+        //
+    }
+}
