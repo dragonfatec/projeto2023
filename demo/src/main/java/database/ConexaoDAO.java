1e5ef02375f6bef69f327d8ab6856b47c4996064
Lukas Fernando <lkfernando12345@gmail.com>
2023-04-28 15:57:59 -0300

    Ideias de como pode ficar alguns metodos da classe ConexaoDAO

--- demo/src/main/java/database/ConexaoDAO.java
+++ demo/src/main/java/database/ConexaoDAO.java
@@ -3,29 +3,26 @@ package database;
 import backend.usuario.Usuario;
 import database.conexao.ConnectionFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.*;
+import java.util.*;
 
 //DAO - Data Acess Object
 public class ConexaoDAO {
-    private ConnectionFactory connection;
+    private final Connection conn;
 
     public ConexaoDAO(){
-        this.connection = new ConnectionFactory();
+        ConnectionFactory connection = new ConnectionFactory();
+        this.conn = connection.recuperaConexao();
     }
 
     public void cadastrarUsuario(String login, String senha, String matricula, String nome, String tipo){
 
         String sql = "INSERT INTO usuario(login, senha, matricula, nome, tipo) VALUES (?, ?, ?, ?, ?);";
 
-        Connection conn = connection.recuperaConexao();
+//        Connection conn = connection.recuperaConexao();
 
         try {
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            PreparedStatement preparedStatement = this.conn.prepareStatement(sql);
 
             preparedStatement.setString(1, login);
             preparedStatement.setString(2, senha);
@@ -40,15 +37,14 @@ public class ConexaoDAO {
     }
 
     public Set<Usuario> usuarioCadastrado(){
-
         Set<Usuario> usuarios = new HashSet<>();
 
         String sql = "SELECT * FROM usuario;";
 
-        Connection conn = connection.recuperaConexao();
+//        Connection conn = connection.recuperaConexao();
 
         try {
-            PreparedStatement ps = conn.prepareStatement(sql);
+            PreparedStatement ps = this.conn.prepareStatement(sql);
             ResultSet resultSet = ps.executeQuery();
 
             while (resultSet.next()){
@@ -57,14 +53,51 @@ public class ConexaoDAO {
                 String matricula = resultSet.getString(4);
                 String nome = resultSet.getString(5);
                 String tipo = resultSet.getString(6);
-
                 usuarios.add(new Usuario(login, senha, matricula, nome, tipo));
-
             }
 
         }catch (SQLException e){
             throw new RuntimeException(e);
         }
         return usuarios;
+    }
+
+
+
+    // Ideias de como pode ficar alguns metodos desta classe
+
+    public Map<String, Arrays> consultar(String sql){
+
+        Map<String, Arrays> resultadoConsulta = new HashMap<>();
+
+        try {
+            PreparedStatement ps = this.conn.prepareStatement(sql);
+            ResultSet resultSet = ps.executeQuery();
+
+            String[] listaNomeColunas = {"id", "nome usuario"};
+
+            for (String nomeColuna : listaNomeColunas) {
+                resultadoConsulta.put(nomeColuna, (Arrays) resultSet.getArray(nomeColuna));
+            }
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return resultadoConsulta;
+    }
+
+    public void inserirDados(String sql){
+        /*
+        Esse metodo vai rodar o sql que você passar como parametro e inserir registros nessa tabela
+
+        Exemplo de como o parametro sql tem que vir:
+            INSERT INTO usuario(login, senha, matricula, nome, tipo) VALUES ('abc12', 12345, 12345678, Nome Teste,Colaborador);
+        */
+        try {
+            PreparedStatement preparedStatement = this.conn.prepareStatement(sql);
+            preparedStatement.execute();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
