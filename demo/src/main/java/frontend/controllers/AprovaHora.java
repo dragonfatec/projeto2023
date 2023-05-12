3d0b02d4a6e4df27086b6900f2fc0946f59f1c6f
alec <alecrondel@hotmail.com>
2023-05-12 19:03:47 -0300

    criação da classe tabelaAprova, e adição de checkboxs

--- demo/src/main/java/frontend/controllers/AprovaHora.java
+++ demo/src/main/java/frontend/controllers/AprovaHora.java
@@ -2,13 +2,16 @@ package frontend.controllers;
 
 import backend.usuario.Usuario;
 import frontend.util.Alerts;
+import frontend.util.TabelaAprova;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class AprovaHora implements Initializable {
@@ -17,13 +20,14 @@ public class AprovaHora implements Initializable {
 
 
     // Table
-    @FXML public TableView tabela;
-    @FXML public TableColumn colunaColaborador;
-    @FXML public TableColumn colunaDataHoraInicial;
-    @FXML public TableColumn colunaDataHoraFinal;
-    @FXML public TableColumn colunaCliente;
-    @FXML public TableColumn colunaHoraExtra;
-    @FXML public TableColumn ColunaSobreaviso;
+    @FXML public TableView<TabelaAprova> tabela;
+    @FXML public TableColumn<TabelaAprova, String> colunaColaborador;
+    @FXML public TableColumn<TabelaAprova, String> colunaDataHoraInicial;
+    @FXML public TableColumn<TabelaAprova, String> colunaDataHoraFinal;
+    @FXML public TableColumn<TabelaAprova, String> colunaCliente;
+    @FXML public TableColumn<TabelaAprova, String> colunaTipo;
+    @FXML public TableColumn<TabelaAprova, String> colunaTotalDeHoras;
+    @FXML public TableColumn<TabelaAprova, CheckBox> colunaSelecione;
 
     // Button
     @FXML public Button btnRegistrarHora;
@@ -35,25 +39,6 @@ public class AprovaHora implements Initializable {
 
     // ChoiceBox
     @FXML public ChoiceBox campoEscolhaEquipe;
-    @FXML public CheckBox checkboxLinha1;
-    @FXML public CheckBox checkboxLinha2;
-    @FXML public CheckBox checkboxLinha3;
-    @FXML public CheckBox checkboxLinha4;
-    @FXML public CheckBox checkboxLinha5;
-    @FXML public CheckBox checkboxLinha6;
-    @FXML public CheckBox checkboxLinha7;
-    @FXML public CheckBox checkboxLinha8;
-    @FXML public CheckBox checkboxLinha9;
-    @FXML public CheckBox checkboxLinha10;
-    @FXML public CheckBox checkboxLinha11;
-    @FXML public CheckBox checkboxLinha12;
-    @FXML public CheckBox checkboxLinha13;
-    @FXML public CheckBox checkboxLinha14;
-    @FXML public CheckBox checkboxLinha15;
-    @FXML public CheckBox checkboxLinha16;
-
-    // Listas
-    ArrayList<CheckBox> listaDeCheckBox = new ArrayList<>();
 
     // Metodos
     public void pegarJustificativa(){
@@ -67,32 +52,31 @@ public class AprovaHora implements Initializable {
     }
 
     public void selecionarTudo(ActionEvent actionEvent) {
-        for (CheckBox cb : listaDeCheckBox){
-            cb.setSelected(true);
-        }
+        
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
-        // Adicionando todos os CheckBox em uma lista
-        listaDeCheckBox.add(checkboxLinha1);
-        listaDeCheckBox.add(checkboxLinha2);
-        listaDeCheckBox.add(checkboxLinha3);
-        listaDeCheckBox.add(checkboxLinha4);
-        listaDeCheckBox.add(checkboxLinha5);
-        listaDeCheckBox.add(checkboxLinha6);
-        listaDeCheckBox.add(checkboxLinha7);
-        listaDeCheckBox.add(checkboxLinha8);
-        listaDeCheckBox.add(checkboxLinha9);
-        listaDeCheckBox.add(checkboxLinha10);
-        listaDeCheckBox.add(checkboxLinha11);
-        listaDeCheckBox.add(checkboxLinha12);
-        listaDeCheckBox.add(checkboxLinha13);
-        listaDeCheckBox.add(checkboxLinha14);
-        listaDeCheckBox.add(checkboxLinha15);
-        listaDeCheckBox.add(checkboxLinha16);
 
         Usuario usuario = Usuario.getInstancia();
+
+        ObservableList<TabelaAprova> listaHorasPendentes = FXCollections.observableArrayList();
+
+        //Dados para Teste
+        listaHorasPendentes.add(new TabelaAprova("Alec", "12/05/2023 18:00", "12/05/2023 19:00", "Americanas", "Sobreaviso", "01:00"));
+        listaHorasPendentes.add(new TabelaAprova("Pedro", "12/05/2023 18:00", "12/05/2023 20:00", "Apple", "Hora Extra", "02:00"));
+        listaHorasPendentes.add(new TabelaAprova("Lucas", "12/05/2023 18:00", "12/05/2023 18:30", "Americanas", "Sobreaviso", "00:30"));
+
+        //Atribuição
+        colunaColaborador.setCellValueFactory(new PropertyValueFactory<TabelaAprova, String>("colaborador"));
+        colunaDataHoraInicial.setCellValueFactory(new PropertyValueFactory<TabelaAprova, String>("dataHoraInicial"));
+        colunaDataHoraFinal.setCellValueFactory(new PropertyValueFactory<TabelaAprova, String>("dataHoraFinal"));
+        colunaCliente.setCellValueFactory(new PropertyValueFactory<TabelaAprova, String>("cliente"));
+        colunaTipo.setCellValueFactory(new PropertyValueFactory<TabelaAprova, String>("tipo"));
+        colunaTotalDeHoras.setCellValueFactory(new PropertyValueFactory<TabelaAprova, String>("totalDeHoras"));
+        colunaSelecione.setCellValueFactory(new PropertyValueFactory<TabelaAprova, CheckBox>("selecione"));
+
+        tabela.setItems(listaHorasPendentes);
 
         textoNomeUsuario.setText("Olá "+ usuario.getNome() + "!");
     }
