5bccc9972c3403b6da45c36d49ee1f08b95291e0
Lukas Fernando <lkfernando12345@gmail.com>
2023-05-28 17:34:44 -0300

    criando as tabelas de equipe

---
+++ demo/src/main/java/frontend/util/TabelaEquipe.java
@@ -0,0 +1,16 @@
+package frontend.util;
+
+import javafx.scene.control.CheckBox;
+
+public class TabelaEquipe {
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
